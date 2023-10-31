34a63c2e1e25d267d0e112ff723fd3e22519d08f
Bibek Regmi <1016matrixbibek@gmail.com>
2023-10-31 14:38:12 +0545

    JWT token create and varift token is added in User service layer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// const UserRepository = require('./repository/user-repository')
+const UserService = require('./services/user-service');
 
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
@@ -19,8 +19,12 @@ const prepareAndStartServer = () => {
     app.listen(PORT, async() => {
         console.log(`Server started on port: ${PORT}`);
 
-        // const repo = new UserRepository();
-        // const response = await repo.getById(3);
+        const service = new UserService();
+        // const newToken = service.createToken({email:'Jon@hotmail.com', id: 1});
+        // console.log("New Token: ", newToken);
+
+        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkpvbkBob3RtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE2OTg3NDE3ODAsImV4cCI6MTY5ODc0MTgxMH0.fpvqRrMMkTos7pJPJdCj4PIM9oLU8jsVO1eT83FOfDg'
+        // const response = service.verifyToken(token);
         // console.log(response);
     });
 
